9c4bb947f77e25e61b45e3fda79a3384842c3be9
Prasanna Kumar <prasannaboga@gmail.com>
2021-11-07 10:39:21 +0530

    Added html files

--- app/web_app.rb
+++ app/web_app.rb
@@ -5,7 +5,7 @@ require "logger"
 require "json"
 require "rack"
 require "sinatra"
-require "socket"
+
 
 # Webapp to sinatra server
 class WebApp < Sinatra::Base
@@ -13,19 +13,22 @@ class WebApp < Sinatra::Base
   enable :logging
 
   set :environment, ENV["APP_ENV"]
-  set :public_folder, "#{File.dirname(__FILE__)}/public"
+  set :public_folder, "#{__dir__}/public"
   set :logger, Logger.new($stdout)
   set :server, :puma
-  set :app_file, __FILE__
 
   configure :development do
     register Sinatra::Reloader
   end
 
   get "/" do
-    server_name = Socket.gethostname
-    logger.info "[server] name :: #{server_name}"
-    content_type :json
-    { status: :ok, app_env: ENV["APP_ENV"] }.to_json
+
+    puts settings.public_folder
+
+    erb :index, locals: {
+      environment: settings.environment,
+      server_start_at: ENV["SERVER_START_AT"],
+      server_name: ENV["SERVER_NAME"],
+    }
   end
 end
--- config.ru
+++ config.ru
@@ -1,9 +1,11 @@
 require "bundler"
 require "dotenv/load"
+require "socket"
 
 Bundler.require
 
-ENV["SERVER_START_TIME"] = DateTime.now.strftime("%Y.%m.%d.%H.%M.%S")
+ENV["SERVER_START_AT"] = DateTime.now.strftime("%Y.%m.%d.%H.%M.%S")
+ENV["SERVER_NAME"] = Socket.gethostname
 
 require "./app/web_app"
 
